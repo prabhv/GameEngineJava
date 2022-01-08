5b127f7f6d9f1b5785c5a24e1a5bfd793874a709
prabhv <prabhan.v.naik@gmail.com>
2022-01-09 00:49:36 +0100

    OpenGL shader implementation and binding

--- src/main/java/testEngine/LevelEditorScene.java
+++ src/main/java/testEngine/LevelEditorScene.java
@@ -1,32 +1,174 @@
 package testEngine;
 
-import java.awt.event.KeyEvent;
+
+import org.lwjgl.BufferUtils;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 
 public class LevelEditorScene extends Scene{
 
-    private boolean changingScene = false;
-    float timeToChangeScene = 2.0f;
+    private String vertexShaderSrc = "#version 330 core\n" +
+            "\n" +
+            "layout (location=0) in vec3 aPos;\n" +
+            "layout (location=1) in vec4 aColor;\n" +
+            "\n" +
+            "out vec4 fColor;\n" +
+            "\n" +
+            "void main()\n" +
+            "{\n" +
+            "    fColor = aColor;\n" +
+            "    gl_Position = vec4(aPos, 1.0);\n" +
+            "}";
+
+    private String fragmentShaderSrc = "#version 330 core\n" +
+            "\n" +
+            "in vec4 fColor;\n" +
+            "\n" +
+            "out vec4 color;\n" +
+            "\n" +
+            "void main()\n" +
+            "{\n" +
+            "    color = fColor;\n" +
+            "}";
+
+    private int vertexID, fragmentID, shaderProgram;
+
+    private float[] vertexArray = {
+            //position              //Color
+            0.5f, -0.5f, 0.0f,      1.0f, 0.0f, 0.0f, 1.0f, //bottomright
+            -0.5f, 0.5f, 0.0f,      0.0f, 1.0f, 0.0f, 1.0f, //top left
+            0.5f, 0.5f, 0.0f,       0.0f, 0.0f, 1.0f, 1.0f, //top right
+            -0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 0.0f, 1.0f, //bottom left
+    };
+
+    //must be in counter clockwise order
+    private int[] elementArray = {
+        /*
+                X       X
+                        ^
+                        |
+                X       X
+         */
+        2, 1, 0, // top right triangle
+        0, 1, 3 //bottom left
+    };
+
+    private int vaoID, vboID, eboID;
 
     public LevelEditorScene(){
-        System.out.println("Inside LevelEditor Scene");
+    }
+
+    @Override
+    public void init(){
+        //compile and link shaders
+
+        //load and compile vertex
+        vertexID = glCreateShader(GL_VERTEX_SHADER);
+
+        //pass shader source code to GPU
+        glShaderSource(vertexID, vertexShaderSrc);
+
+        glCompileShader(vertexID);
+
+        //Check for errors in compilation
+        int success = glGetShaderi(vertexID, GL_COMPILE_STATUS);
+        if (success == GL_FALSE){
+            int len = glGetShaderi(vertexID, GL_INFO_LOG_LENGTH);
+            System.out.println("Error: 'defaultshader.glsl'\n \tVertex shader compilation failed");
+            System.out.println(glGetShaderInfoLog(vertexID, len));
+            assert false : " ";
+        }
+
+        //load and compile vertex
+        fragmentID = glCreateShader(GL_FRAGMENT_SHADER);
+
+        //pass shader source code to GPU
+        glShaderSource(fragmentID, fragmentShaderSrc);
+
+        glCompileShader(fragmentID);
+
+        //Check for errors in compilation
+        success = glGetShaderi(fragmentID, GL_COMPILE_STATUS);
+        if (success == GL_FALSE){
+            int len = glGetShaderi(fragmentID, GL_INFO_LOG_LENGTH);
+            System.out.println("Error: 'defaultshader.glsl'\n \tFragment shader compilation failed");
+            System.out.println(glGetShaderInfoLog(fragmentID, len));
+            assert false : " ";
+        }
+
+        //link shaders
+        shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexID);
+        glAttachShader(shaderProgram, fragmentID);
+        glLinkProgram(shaderProgram);
+
+        //check for linking errors
+        success = glGetProgrami(shaderProgram, GL_LINK_STATUS);
+        if (success == GL_FALSE){
+            int len = glGetProgrami(shaderProgram, GL_INFO_LOG_LENGTH);
+            System.out.println("Error: 'defaultshader.glsl'\n \tshader linking failed");
+            System.out.println(glGetProgramInfoLog(shaderProgram, len));
+            assert false : " ";
+        }
+
+        //generate vao, vbo and EBO buffer objects
+
+        vaoID = glGenVertexArrays();
+        glBindVertexArray(vaoID);
+
+        // create a float buffer of vertices
+
+        FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertexArray.length);
+        vertexBuffer.put(vertexArray).flip();
+
+        // create VBO
+        vboID = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
+
+        // create indices
+
+        IntBuffer elementBuffer = BufferUtils.createIntBuffer(elementArray.length);
+        elementBuffer.put(elementArray).flip();
+
+        eboID = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
+
+        //add vertext attribute pointers
+        int positionsSize = 3;
+        int colorSize = 4;
+        int floatSizeBytes = 4;
+        int vertexSizeBytes = (positionsSize + colorSize) * floatSizeBytes;
+
+        glVertexAttribPointer(0, positionsSize, GL_FLOAT, false, vertexSizeBytes, 0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionsSize*floatSizeBytes);
+        glEnableVertexAttribArray(1);
     }
 
     @Override
     public void update(float dt) {
-        System.out.println("'" + (1.0f / dt) + "FPS");
+        //bind shader program
+        glUseProgram(shaderProgram);
+        // bind the vao
+        glBindVertexArray(vaoID);
+        // enable vertext attribue pointer
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
 
-        if (!changingScene && KeyListeners.isKeyPressed(KeyEvent.VK_SPACE)){
-            changingScene = true;
-        }
+        glDrawElements(GL_TRIANGLES, elementArray.length, GL_UNSIGNED_INT, 0);
 
-        if(changingScene && timeToChangeScene > 0){
-            timeToChangeScene -= dt;
-            Window.get().r -= dt * 5.0f;
-            Window.get().g -= dt * 5.0f;
-            Window.get().b -= dt * 5.0f;
-        }
-        else if (changingScene){
-            Window.changeScene(1);
-        }
+        //unbind everything
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+        glBindVertexArray(0);
+        glUseProgram(0);
     }
 }
