93ad91b3e0faf39bc749d0ebc3467eb9938204e7
prabhv <prabhan.v.naik@gmail.com>
2022-01-12 00:01:14 +0100

    Resourcemanagement with Asset pool for containing created objects like textures and shaders

--- src/main/java/testEngine/LevelEditorScene.java
+++ src/main/java/testEngine/LevelEditorScene.java
@@ -4,6 +4,7 @@ package testEngine;
 import components.SpriteRenderer;
 import org.joml.Vector2f;
 import org.joml.Vector4f;
+import utils.AssetPool;
 
 public class LevelEditorScene extends Scene{
 
@@ -33,7 +34,11 @@ public class LevelEditorScene extends Scene{
                 this.addGameObjectToScene(go);
             }
         }
+        loadResources();
+    }
 
+    private void loadResources(){
+        AssetPool.getShader("assets/shaders/default.glsl");
     }
 
     @Override
